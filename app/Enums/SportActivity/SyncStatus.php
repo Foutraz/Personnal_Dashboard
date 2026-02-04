d23eae0ba8b31a3e0c149419296a4b27cd04c596
Quentin <quentin.mari0409@gmail.com>
2026-02-04 23:39:18 +0100

    SportActivity Enums

---
+++ app/Enums/SportActivity/SyncStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums\SportActivity;
+
+enum SyncStatus
+{
+    case OK;
+    case FAILED;
+    case UNKNOWN;
+}
---
+++ app/Enums/SportActivity/Type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums\SportActivity;
+
+enum Type
+{
+    case RUNNING;
+    case SWIMMING;
+    case SKI;
+    case CYCLING;
+    case HIKING;
+}
