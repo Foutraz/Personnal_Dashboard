331bb8e9ec87d689fe144803afd939edea53dab1
Quentin <quentin.mari0409@gmail.com>
2026-02-04 23:50:41 +0100

    migration

---
+++ database/migrations/2026_02_04_220713_create_sport_activity_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // TODO make the sport activity table migration
+    public function up(): void
+    {
+        Schema::create('sport_activity', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sport_activity');
+    }
+};
