fb66f4b63114b04ea4a4ff62ecd8153abc5444c7
Quentin <quentin.mari0409@gmail.com>
2026-02-04 23:51:23 +0100

    SportActivity Seeding

---
+++ database/seeders/SportActivitySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SportActivity;
+use Illuminate\Database\Seeder;
+
+class SportActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SportActivity::factory(5)->create();
+    }
+}
