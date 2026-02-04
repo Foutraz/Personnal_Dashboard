fd0e8dc00404671afd159fae2384490f2b6197b1
Quentin <quentin.mari0409@gmail.com>
2026-02-04 23:51:06 +0100

    migration

---
+++ database/factories/SportActivityFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SportActivity;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<SportActivity>
+ */
+class SportActivityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // TODO make the factory
+        ];
+    }
+}
