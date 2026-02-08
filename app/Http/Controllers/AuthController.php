c90e42c56dcc828dcf8e3259d9a933a7ca2e4167
Quentin <quentin.mari0409@gmail.com>
2026-02-08 23:11:30 +0100

    fix AuthController.php
    Signed-off-by: Quentin <quentin.mari0409@gmail.com>

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -28,7 +29,7 @@ class AuthController extends Controller
             'password' => Hash::make($data['password']),
         ]);
 
-        $token = auth('api')->login($user);
+        $token = JWTAuth::fromUser($user);
 
         return $this->respondWithToken($token);
     }
