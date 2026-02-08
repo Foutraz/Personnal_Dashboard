466dfa0b431657579e73187715a93f2db2ef2778
Quentin <quentin.mari0409@gmail.com>
2026-02-08 23:06:58 +0100

    secure routes
    Signed-off-by: Quentin <quentin.mari0409@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Rest\Controllers\SportActivitiesController;
 use App\Rest\Controllers\UsersController;
 use Lomkit\Rest\Facades\Rest;
 
-Rest::resource('users', UsersController::class);
-Rest::resource('sportActivities', SportActivitiesController::class);
+Route::middleware('auth:api')->group(function () {
+    Rest::resource('users', UsersController::class);
+    Rest::resource('sportActivities', SportActivitiesController::class);
+    Route::prefix('auth')->group(function () {
+        Route::post('refresh',  [AuthController::class, 'refresh']);
+        Route::post('logout',   [AuthController::class, 'logout']);
+        Route::get('me',        [AuthController::class, 'me']);
+    });
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+});
