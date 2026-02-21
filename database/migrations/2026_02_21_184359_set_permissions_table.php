69cbe3c1635130d494ffd6238233e17e303193c8
Quentin <quentin.mari0409@gmail.com>
2026-02-21 19:13:13 +0100

    make perms migration
    Signed-off-by: Quentin <quentin.mari0409@gmail.com>

---
+++ database/migrations/2026_02_21_184359_set_permissions_table.php
@@ -0,0 +1,80 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        $modelKeys = array_keys(config('access_models', []));
+
+        DB::transaction(function () use ($modelKeys) {
+            $abilities = [
+                'viewAny',
+                'view',
+                'create',
+                'update',
+                'delete',
+                'sync',
+                'import',
+            ];
+            $perimeters = ['global', 'own'];
+            $guard = 'api';
+            foreach ($modelKeys as $modelKey) {
+                foreach ($perimeters as $perimeter) {
+                    foreach ($abilities as $ability) {
+                        $name = "$ability.$perimeter.$modelKey";
+                        Permission::findOrCreate($name, $guard);
+                    }
+                }
+            }
+
+            $specials = ['connexion', 'settings'];
+            foreach ($specials as $special) {
+                foreach ($perimeters as $perimeter) {
+                    $name = "manage.$perimeter.$special";
+                    Permission::findOrCreate($name, $guard);
+                }
+            }
+        });
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
+
+    public function down(): void
+    {
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        $modelKeys = array_keys(config('access_models', []));
+
+        DB::transaction(function () use ($modelKeys) {
+            $abilities = ['viewAny', 'view', 'create', 'update', 'delete', 'sync', 'import'];
+            $perimeters = ['global', 'own'];
+            $guard = 'api';
+            foreach ($modelKeys as $modelKey) {
+                foreach ($perimeters as $perimeter) {
+                    foreach ($abilities as $ability) {
+                        Permission::query()->where('name', "$ability.$perimeter.$modelKey")
+                            ->where('guard_name', $guard)
+                            ->delete();
+                    }
+                }
+            }
+
+            foreach (['connexion','settings'] as $special) {
+                foreach ($perimeters as $perimeter) {
+                    Permission::query()->where('name', "manage.$perimeter.$special")
+                        ->where('guard_name', $guard)
+                        ->delete();
+                }
+            }
+        });
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
+};
